2a30eb8936c330921346634be47d822f25a883e5
Elaabouafoura <elaa.bouafoura@esprit.tn>
2026-06-18 14:30:32 +0100

    fix firebase

--- frontend/src/views/auth/SignIn/components/OauthSignIn.tsx
+++ frontend/src/views/auth/SignIn/components/OauthSignIn.tsx
@@ -1,9 +1,6 @@
 import Button from '@/components/ui/Button'
 import { useAuth } from '@/auth'
-import {
-    apiGoogleOauthSignIn,
-    apiGithubOauthSignIn,
-} from '@/services/OAuthServices'
+
 
 type OauthSignInProps = {
     setMessage?: (message: string) => void
@@ -13,46 +10,16 @@ type OauthSignInProps = {
 const OauthSignIn = ({ setMessage, disableSubmit }: OauthSignInProps) => {
     const { oAuthSignIn } = useAuth()
 
-    const handleGoogleSignIn = async () => {
-        if (!disableSubmit) {
-            oAuthSignIn(async ({ redirect, onSignIn }) => {
-                try {
-                    const resp = await apiGoogleOauthSignIn()
-                    if (resp) {
-                        const { token, user } = resp
-                        onSignIn({ accessToken: token }, user)
-                        redirect()
-                    }
-                } catch (error) {
-                    setMessage?.((error as string)?.toString() || '')
-                }
-            })
-        }
-    }
+   
 
-    const handleGithubSignIn = async () => {
-        if (!disableSubmit) {
-            oAuthSignIn(async ({ redirect, onSignIn }) => {
-                try {
-                    const resp = await apiGithubOauthSignIn()
-                    if (resp) {
-                        const { token, user } = resp
-                        onSignIn({ accessToken: token }, user)
-                        redirect()
-                    }
-                } catch (error) {
-                    setMessage?.((error as string)?.toString() || '')
-                }
-            })
-        }
-    }
+    
 
     return (
         <div className="flex items-center gap-2">
             <Button
                 className="flex-1"
                 type="button"
-                onClick={handleGoogleSignIn}
+             
             >
                 <div className="flex items-center justify-center gap-2">
                     <img
@@ -66,7 +33,7 @@ const OauthSignIn = ({ setMessage, disableSubmit }: OauthSignInProps) => {
             <Button
                 className="flex-1"
                 type="button"
-                onClick={handleGithubSignIn}
+               
             >
                 <div className="flex items-center justify-center gap-2">
                     <img
